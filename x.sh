c58a865395a3b467dd0f6f7b590673082863da2b
zhangxiaojian <zhangxj@tuya.com>
2023-04-26 10:47:59 +0800

    feat: alias

---
+++ x.sh
@@ -0,0 +1,5 @@
+alias c=clear
+alias og="open https://google.com"
+alias ob="open https://www.baidu.com"
+alias gpush='echo -n Enter your feat: && read feat && echo your feat is : $feat && cd $PWD && git add . && git commit -m "feat: $feat" && git push && echo push success!'
+
